494b7047b76edfeef9a81467407011da19074c6f
Abraham Bueno <abueno@localhost.localdomain>
2018-06-15 11:45:58 -0500

    Completed project requirements. It has the new instructions and it prints the correct output of 1 4 and 5.

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -1,19 +1,57 @@
 /**
  * LS-8 v2.0 emulator skeleton code
- */
+*/
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
+
+//  Print correct values
 const LDI = 0b10011001;
 const PRN = 0b01000011;
-const MUL = 0b10101010;
 const HLT = 0b00000001;
+const MUL = 0b10101010;
+
+// Stack
 const PUSH = 0b01001101;
 const POP = 0b01001100;
+
+// Subroutine call
 const CALL = 0b01001000;
 const RET = 0b00001001;
 const ADD = 0b10101000;
+
+// Interrupt
+const ST = 0b10011010;
+const JMP = 0b01010000;
+const PRA = 0b01000010;
+const IRET = 0b00001011;
+const CMP = 0b10100000;
+const JEQ = 0b01010001;
+const JNE = 0b01010010;
+
+// Step 8 progress
+//  const branchTable = [];
+
+//  function LDI_Handler(regA, regB) {
+//     this.reg[regA] = regB;
+//  }
+//  function PRN_Handler(regA) {
+//     console.log(this.reg[regA]);
+//  }
+//  function HLT_Handler() {
+//     this.stopClock();
+//  }
+//  function MULT_Handler(regA, regB) {
+//     this.reg[regA] = this.reg[regA] * this.reg[regB]
+//  }
+
+//  branchTable['LDI'] = LDI_Handler
+//  branchTable['PRN'] = PRN_Handler
+//  branchTable['HLT'] = HLT_Handler
+//  branchTable['MULT'] = MULT_Handler
+//  console.log(branchTable)
+
 class CPU {
 
     /**
@@ -26,9 +64,16 @@ class CPU {
 
         // Special-purpose registers
         this.PC = 0; // Program Counter
-        this.SP = 244;
-        this.newStack = new Stack;
+        this.IM = 5; // R5, interrupt mask 11111111 /8; 11110000/4 X.X.X.A/class C network
+        this.IS = 6; // R6, interrupt status
+        this.SP = 7; // 7 = R7, SP = stack pointer
+        this.FL = 0b00000000;
 
+        // Register
+        //this.FL = 0; // R0-R7, R5-R7 is reserved
+
+        this.reg[this.SP] = 0xF4; // 244 = F4
+        this.reg[this.IM] = 0xF8; // 248 = F8
     }
 
     /**
@@ -66,47 +111,107 @@ class CPU {
      */
     alu(op, regA, regB) {
         switch (op) {
-            case MUL:
+            case ADD:
+                this.reg[regA] = this.reg[regA] + this.reg[regB];
                 this.PC += 3;
-                let valA = this.reg[regA];
-                let valB = this.reg[regB];
-                let prod = valA * valB;
-                this.reg[regA] = prod;
                 break;
-            // !!! IMPLEMENT ME
-            case LDI:
-                this.reg[regA] = regB//10011001;
+            case MUL:
+                this.reg[regA] = this.reg[regA] * this.reg[regB];
                 this.PC += 3;
+                break;
+            case LDI:
+                // console.log('reg', this.reg)
+                this.reg[regA] = regB;
+                this.PC += 3;
+                // console.log('reg', this.reg)
                 break;
             case PRN:
-                this.PC += 2;
                 console.log(this.reg[regA]);
-                break;
-            case PUSH:
                 this.PC += 2;
-                this.newStack.push(this.reg[regA]);
-                break;
-            case POP:
-                this.PC += 2;
-                this.reg[regA] = this.newStack.pop()
-
                 break;
             case HLT:
-                this.stopClock()
+                this.stopClock();
+                break;
+            case PUSH:
+                // this.SP--;
+                // console.log('reg', this.reg)
+                this.reg[this.SP] -= 1;
+                this.ram.write(this.reg[this.SP], this.reg[regA])
+                this.PC += 2;
+                // console.log('reg', this.reg)
+                // console.log('register before decrement', this.reg)
+                // console.log('after decrementing', this.reg)
+                break;
+            case POP:
+                this.reg[regA] = this.ram.read(this.reg[this.SP]);
+                // this.SP++;
+                this.PC += 2;
+                // console.log('reg', this.reg)
+                // console.log('before increment', this.reg)
+                this.reg[this.SP] += 1;
+                // console.log('reg', this.reg)
+                break;
             case CALL:
-                this.newStack.push(this.PC + 2);
+                this.reg[this.SP] -= 1;
+                this.ram.write(this.reg[this.SP], this.PC + 2) // The address of the _next_ instruction that will execute is pushed onto the stack.
                 this.PC = this.reg[regA]
-                // console.log('called, reg', this.reg, this.PC, this.ram.read(this.PC))
                 break;
             case RET:
-                // console.log('returned, reg', this.reg)
-                this.PC = this.newStack.pop();
+                this.PC = this.ram.read(this.reg[this.SP]);
+                this.reg[this.SP] += 1;
+
                 break;
-            case ADD:
-                this.reg[regA] = this.reg[regA] * 2;
+            case ST:
+                this.reg[this.SP] -= 1;
+                this.ram.write(this.reg[regA], this.reg[regB])
+
                 this.PC += 3;
                 break;
-            // break;
+            case JMP:
+                this.PC = this.ram.read(this.reg[regA]);
+                // console.log('jump infinite')
+                break;
+            case PRA:
+
+                console.log(String.fromCharCode(this.reg[regA])) // String.fromCharCode
+                this.PC += 2;
+                break;
+            case CMP:
+                // `FL` bits: `00000LGE` ; it's internal
+                if (this.reg[regA] > this.reg[regB]) {
+                    this.FL = 0b00000010
+                } else if (this.reg[regA] === this.reg[regB]) {
+                    this.FL = 0b00000001
+                } else {
+                    this.FL = 0b00000100
+                }
+                this.PC += 3;
+                // console.log('comparison, FL: ', this.FL)
+                break;
+            case JEQ:
+                // console.log('JEQ, before this.PC', this.PC)
+                if (this.FL === 0b00000001) {
+                    this.PC = this.reg[regA];
+                } else {
+                    this.PC += 2;
+                }
+                // console.log('JEQ, after this.PC', this.PC)
+                break;
+            case JNE:
+                // console.log('JNE, before this.PC', this.PC)
+                if (this.FL !== 0b00000001) {
+                    this.PC = this.reg[regA];
+                } else {
+                    this.PC += 2;
+                }
+                // console.log('JNE, after this.PC', this.PC)
+                break;
+            case IRET:
+                // console.log('iret executed')
+                break;
+            default:
+                this.stopClock();
+                break;
         }
     }
 
@@ -114,35 +219,48 @@ class CPU {
      * Advances the CPU one cycle
      */
     tick() {
-        let IR = this.ram.read(this.PC)//.toString(2)//[this.PC];
-        // if (IR === 168) {
-        //     console.log('168 confirmed', IR)
-        // }
-        let operandA = this.ram.read(this.PC + 1);
-        let operandB = this.ram.read(this.PC + 2);
-        this.alu(IR, operandA, operandB)
-
-
         // Load the instruction register (IR--can just be a local variable here)
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
         // right now.)
 
         // !!! IMPLEMENT ME
+        const IR = this.ram.read(this.PC);
 
         // Debugging output
-        // console.log(IR)
         // console.log(`${this.PC}: ${IR.toString(2)}`);
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
 
         // !!! IMPLEMENT ME
+        const operandA = this.ram.read(this.PC + 1);
+        const operandB = this.ram.read(this.PC + 2);
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
-        // !!! IMPLEMENT ME
+        // Step 2-5, switch case in tick
+        // switch(IR) {
+        //     case LDI: 
+        //         this.reg[operandA] = operandB;
+        //         this.PC += 3;
+        //         break;
+        //     case PRN: 
+        //         console.log(this.reg[operandA]);
+        //         this.PC += 2;
+        //         break;
+        //     case HLT: 
+        //         this.stopClock();
+        //         break;
+        //     default:
+        //         this.stopClock();
+        //         return;
+        // }
+
+        // Multiply - Step 6
+        this.alu(IR, operandA, operandB)
+        // branchTable[IR](operandA, operandB)
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
@@ -150,51 +268,8 @@ class CPU {
         // for any particular instruction.
 
         // !!! IMPLEMENT ME
-    }
-}
-class Stack {
-
-    // Array is used to implement stack
-    constructor() {
-        this.items = [];
-        this.SP = 244; // Stack program counter
-
-    }
-
-    // Functions to be implemented
-    // push(item)
-    push(element) {
-        // push element into the items
-        this.items[this.SP - 1] = element;
-        this.SP--;
-    }
-    // pop()
-    pop() {
-        // return top most element in the stack
-        // and removes it from the stack
-        // Underflow if stack is empty
-        if (this.items.length == 0)
-            return "Underflow";
-        this.SP++
-        return this.items.pop();
-    }
-    // peek()
-    peek() {
-        // return the top most element from the stack
-        // but does'nt delete it.
-        return this.items[this.items.length - 1];
-    }
-    // isEmpty()
-    isEmpty() {
-        // return true if stack is empty
-        return this.items.length == 0;
-    }
-    // printStack()
-    printStack() {
-        var str = "";
-        for (var i = 0; i < this.items.length; i++)
-            str += this.items[i] + " ";
-        return str;
+        // const instLen = (IR >> 6) + 1;
+        // this.PC += instLen;
     }
 }
 
